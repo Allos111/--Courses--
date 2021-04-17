33c9dbb9b13f210021b2689119774a34ad05ed5b
Toussaint Will Mayala <wtmayala@gmail.com>
2021-04-17 23:44:37 +0200

    Update HelloUCSDWorld.java
    More Map providers added

--- Object-Oriented-Java-programming-Data-Stuctures-and-Beyond/UCSDUnfoldingMaps/src/HelloUCSDWorld.java
+++ Object-Oriented-Java-programming-Data-Stuctures-and-Beyond/UCSDUnfoldingMaps/src/HelloUCSDWorld.java
@@ -10,21 +10,35 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
  * This is the basic stub to start creating interactive maps.
  */
 public class HelloUCSDWorld extends PApplet {
+	
+	UnfoldingMap googleMap;
+	UnfoldingMap microsoftAerialMap;
+	UnfoldingMap microsoftRoadMap;
+	UnfoldingMap microsoftHybridMap;
+	UnfoldingMap currentMap;
 
-	UnfoldingMap map;
 
 	public void setup() {
 		size(800, 600, OPENGL);
 
-		map = new UnfoldingMap(this, new Google.GoogleTerrainProvider());
-		map.zoomAndPanTo(14, new Location(32.881, -117.238)); // UCSD
+		googleMap = new UnfoldingMap(this, new Google.GoogleTerrainProvider());
+		
+		microsoftAerialMap = new UnfoldingMap(this, new Microsoft.AerialProvider());
+		
+		microsoftRoadMap = new UnfoldingMap(this, new Microsoft.RoadProvider());
+		
+		microsoftHybridMap = new UnfoldingMap(this, new Microsoft.HybridProvider());
+		
+		currentMap = microsoftHybridMap;
+		
+		currentMap.zoomAndPanTo(14, new Location(32.881, -117.238)); // UCSD Location
 
-		MapUtils.createDefaultEventDispatcher(this, map);
+		MapUtils.createDefaultEventDispatcher(this, currentMap);
 	}
 
 	public void draw() {
 		background(0);
-		map.draw();
+		currentMap.draw();
 	}
 
 }
